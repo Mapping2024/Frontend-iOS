e41442cfa303868fe1f7794b665eb8ee061e4b91
Minnnning <6alswjd6@gmail.com>
2024-11-02 19:48:28 +0900

    first commit

---
+++ Mapping/MappingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MappingApp.swift
+//  Mapping
+//
+//  Created by 김민정 on 11/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MappingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
