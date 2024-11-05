7628d8e00bddc30a534b26a6fe23918c25e5c310
Minnnning <6alswjd6@gmail.com>
2024-11-05 23:08:00 +0900

    modify: 지도 출력과 내위치 출력 로직 변경

--- Mapping/ContentView.swift
+++ Mapping/ContentView.swift
@@ -6,25 +6,113 @@
 //
 
 import SwiftUI
+import MapKit
+import CoreLocationUI
+
+//struct ContentView: View {
+//    @EnvironmentObject var userManager: UserManager
+//    
+//    var body: some View {
+//        TabView {
+//            MapView()
+//                .tabItem {
+//                    Image(systemName: "map.fill")
+//                    Text("지도")
+//                }
+//            
+//            MyInfo()
+//                .tabItem {
+//                    Image(systemName: "person.fill")
+//                    Text("내 정보")
+//                }
+//        }
+//        .font(.headline)
+//    }
+//}
+
+//struct ContentView: View {
+//    
+//    var body: some View {
+//        Map {
+//            Marker("San Francisco City Hall", coordinate: CLLocationCoordinate2D(latitude: 37.33403809906777, longitude: -122.00932605416199))
+//                .tint(.orange)
+//            Annotation("San Francisco Public Library", coordinate: CLLocationCoordinate2D(latitude: 37.3359148165279, longitude: -122.00987322477263)) {
+//                ZStack {
+//                    Button(action: {//여기 상세보기 하는 모달창 로직
+//                    }) {
+//                        Image(systemName: "trash.fill")
+//                    }
+//                }
+//            }
+//            Annotation("Diller Civic Center Playground",  coordinate: CLLocationCoordinate2D(latitude: 37.33512148259535, longitude: -122.00750215212648)) {
+//                ZStack {
+//                    RoundedRectangle(cornerRadius: 5)
+//                        .fill(Color.yellow)
+//                    Text("🛝")
+//                        .padding(5)
+//                }
+//            }
+//        }
+//        .mapControlVisibility(.hidden)
+//    }
+//}
+
+//struct ContentView: View {
+//    @State private var position: MapCameraPosition = .userLocation(fallback: .automatic) // 마커 결과에 따른 처음 카메라 조정 Map에 매개변수 넣야함
+//    @StateObject private var locationManager = LocationManager()
+//    @State private var region = MKCoordinateRegion(
+//        center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194),
+//        span: MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001)
+//    )
+//    
+//    var body: some View {
+//        Map(position: $position) {
+//            Marker("San Francisco City Hall", systemImage: "trash.fill", coordinate: CLLocationCoordinate2D(latitude: 37.33403809906777, longitude: -122.00932605416199)).tint(.gray)
+//            Marker("Diller Civic Center Playground", systemImage: "trash.fill", coordinate: CLLocationCoordinate2D(latitude: 37.33512148259535, longitude: -122.00750215212648)).tint(.black)
+//        }
+//        .onAppear {
+//            locationManager.requestLocation()
+//        }
+//        .onChange(of: locationManager.userLocation) { oldLocation, newLocation in
+//                    // 새로운 위치 정보를 받아올 때마다 지도 중심을 업데이트합니다.
+//                    if let newLocation = newLocation {
+//                        region.center = newLocation.coordinate
+//                    }
+//                }
+//        .mapControls {
+//            MapUserLocationButton()
+//            MapCompass()
+//            MapScaleView()
+//        }
+//        //.mapStyle(.standard(elevation: .realistic))
+//    }
+//}
+
+import CoreLocationUI
 
 struct ContentView: View {
-    @EnvironmentObject var userManager: UserManager
+    @StateObject private var locationManager = LocationManager()
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194),
+        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+    )
     
     var body: some View {
-        TabView {
-            MapView()
-                .tabItem {
-                    Image(systemName: "map.fill")
-                    Text("지도")
+        Map(coordinateRegion: $region, showsUserLocation: true)
+            .ignoresSafeArea()
+            .onAppear {
+                locationManager.requestLocation()
+            }
+            .onChange(of: locationManager.userLocation) { oldLocation, newLocation in
+                if let newLocation = newLocation {
+                    region.center = newLocation.coordinate
                 }
-            
-            MyInfo()
-                .tabItem {
-                    Image(systemName: "person.fill")
-                    Text("내 정보")
-                }
-        }
-        .font(.headline)
+            }
+            .mapControls {
+                MapUserLocationButton()
+                MapCompass()
+                MapScaleView()
+            }
     }
 }
 
--- Mapping/LocationManager.swift
+++ Mapping/LocationManager.swift
@@ -1,37 +1,98 @@
+////
+////  LocationManager.swift
+////  Mapping
+////
+////  Created by 김민정 on 11/3/24.
+////
 //
-//  LocationManager.swift
-//  Mapping
-//
-//  Created by 김민정 on 11/3/24.
-//
-
 import SwiftUI
 import MapKit
 import CoreLocation
+//
+//class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+//    private let manager = CLLocationManager()
+//    @Published var region = MKCoordinateRegion(
+//        center: CLLocationCoordinate2D(latitude: 36.628504, longitude: 127.457493),
+//        span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+//    )
+//    
+//    override init() {
+//        super.init()
+//        manager.delegate = self
+//        manager.desiredAccuracy = kCLLocationAccuracyBest
+//    }
+//    
+//    func requestLocationPermission() {
+//        manager.requestWhenInUseAuthorization()
+//    }
+//    
+//    func startUpdatingLocation() {
+//        manager.startUpdatingLocation()
+//    }
+//    
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        guard let location = locations.last else { return }
+//        region.center = location.coordinate
+//    }
+//}
+
+
+//class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+//    private let manager = CLLocationManager()
+//    @Published var userLocation: CLLocation?
+//    
+//    override init() {
+//        super.init()
+//        manager.delegate = self
+//        manager.desiredAccuracy = kCLLocationAccuracyBest
+//        manager.requestWhenInUseAuthorization()
+//    }
+//    
+//    func requestLocation() {
+//        manager.startUpdatingLocation()
+//    }
+//    
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        guard let location = locations.last else { return }
+//        userLocation = location
+//        manager.stopUpdatingLocation()
+//    }
+//    
+//    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+//            print(error)
+//    }
+//    
+//}
 
 class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
-    private let manager = CLLocationManager()
-    @Published var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 37.33483144444238, longitude: -122.00887544345031),
-        span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
-    )
+    private var locationManager = CLLocationManager()
+    
+    @Published var authorizationStatus: CLAuthorizationStatus
+    @Published var userLocation: CLLocation?
     
     override init() {
+        self.authorizationStatus = locationManager.authorizationStatus
         super.init()
-        manager.delegate = self
-        manager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
     }
     
-    func requestLocationPermission() {
-        manager.requestWhenInUseAuthorization()
+    func requestLocation() {
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
     
-    func startUpdatingLocation() {
-        manager.startUpdatingLocation()
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        authorizationStatus = manager.authorizationStatus
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else { return }
-        region.center = location.coordinate
+        if let location = locations.last {
+            userLocation = location
+            locationManager.stopUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error updating location: \(error)")
     }
 }
