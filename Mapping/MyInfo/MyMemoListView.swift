2f1494f884a916ee6934495a19671c80993b2aab
Minnnning <6alswjd6@gmail.com>
2024-11-07 17:15:25 +0900

    feat: 마이페이지 생성

---
+++ Mapping/MyInfo/MyMemoListView.swift
@@ -0,0 +1,18 @@
+//
+//  MyMemoListView.swift
+//  Mapping
+//
+//  Created by 김민정 on 11/7/24.
+//
+
+import SwiftUI
+
+struct MyMemoListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyMemoListView()
+}
