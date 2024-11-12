948b48b177f4940c35240ccd6358ad71a3647a05
Minjeong Kim <80758613+Minnnning@users.noreply.github.com>
2024-11-12 12:55:37 +0900

    feat: 내 메모 리스트 뷰 기능 (#10)

--- Mapping/MyInfo/MyMemoListView.swift
+++ Mapping/MyInfo/MyMemoListView.swift
@@ -6,13 +6,110 @@
 //
 
 import SwiftUI
+import Alamofire
 
 struct MyMemoListView: View {
+    @EnvironmentObject var userManager: UserManager
+    @State private var myMemo: [MyMemo] = []
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List(myMemo) { memo in
+                VStack(alignment: .leading, spacing: 8) {
+                    HStack{
+                        VStack(alignment: .leading){
+                            Text(memo.title)
+                                .font(.headline)
+                                .foregroundColor(.primary)
+                            
+                            Text(memo.content)
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                                .lineLimit(2) // 줄 수 제한
+                        }
+                        Spacer()
+                        if memo.images.first != nil {
+                            AsyncImage(url: URL(string: memo.images.first!)) { image in
+                                image.resizable()
+                            } placeholder: {
+                                ProgressView()
+                            }
+                            .frame(width: 50,height: 50)
+                            .cornerRadius(10)
+                            //.padding(.horizontal)
+                        }
+                    }
+                    HStack {
+                        Text("\(memo.category)")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                        
+                        Spacer()
+                        
+                        HStack {
+                            HStack {
+                                Image(systemName: "hand.thumbsup.fill")
+                                Text("\(memo.likeCnt)")
+                            }
+                            
+                            HStack {
+                                Image(systemName: "hand.thumbsdown.fill")
+                                Text("\(memo.hateCnt)")
+                            }
+                        }
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                    }
+                }
+            }
+            .navigationBarTitle("내 메모")
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    fetchMyMemo()
+                }
+            }
+        }
     }
+    
+    private func fetchMyMemo() {
+        let accessToken = userManager.accessToken
+        let headers: HTTPHeaders = ["Authorization": "Bearer \(accessToken)"]
+        let url = "https://api.mapping.kro.kr/api/v2/memo/my-memo"
+        
+        AF.request(url, method: .get, headers: headers).responseDecodable(of: MyMemoResponse.self) { response in
+            switch response.result {
+            case .success(let memoResponse):
+                if memoResponse.success {
+                    self.myMemo = memoResponse.data
+                    //print("Successfully fetched memo locations: \(memoResponse.data)")
+                } else {
+                    print("Failed to fetch memo locations: \(memoResponse.message)")
+                }
+            case .failure(let error):
+                print("Error fetching memo locations: \(error)")
+            }
+        }
+    }
+}
+
+struct MyMemo: Identifiable, Decodable {
+    let id: Int
+    let title: String
+    let content: String
+    let category: String
+    let likeCnt: Int
+    let hateCnt: Int
+    let images: [String]
+}
+
+struct MyMemoResponse: Decodable {
+    let status: Int
+    let success: Bool
+    let message: String
+    let data: [MyMemo]
 }
 
 #Preview {
     MyMemoListView()
+        .environmentObject(UserManager())
 }
